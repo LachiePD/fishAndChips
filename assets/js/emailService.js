e1ec2b089766119963be3ff31d7353bde7c26d42
Lachie David <lachiepd@gmail.com>
2023-04-23 23:41:52 +1000

    if this works first time... geewiz

---
+++ assets/js/emailService.js
@@ -0,0 +1,30 @@
+const form = document.getElementById('contact-form');
+
+
+form.addEventListener('submit', async(event)=>{
+	event.preventDefault();
+	const formData = new FormData(form);
+	const	name	=	formData.get('name');
+	const	email	=	formData.get('email');
+	const	message	=	formData.get('message');
+
+
+	try{
+
+		const response	=	await fetch(process.env.SERVICE_URL,{
+			method:'POST',
+			headers:{
+				'Content-Type':'application/json'
+			},
+			body: JSON.stringify({name, email, message})
+		});
+		if(response.ok){
+			console.log('server connected');
+		}else{
+			console.log('error when fetching server response')
+		}
+	}catch(err){
+		console.log('Error sending mail. Error: ' + err);
+
+	}
+});
